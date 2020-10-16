ab1b4b9c7fdb405cac24afb07414c536a3b508a3
Christopher Sardegna <gpg@reagentx.net>
2020-10-16 15:43:46 -0400

    Formatting, use mutable reference

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,13 @@ use std::time::Instant;
 fn main() {
     let before = Instant::now();
     let grid = generate_grid(10000, 10000);
-    //     let grid = vec![
-    //         vec![1, 1, 1, 1, 1, 1, 1],
-    //         vec![1, 1, 1, 1, 1, 1, 1],
-    //         vec![1, 1, 1, 1, 1, 1, 1],
-    //         vec![1, 1, 1, 1, 1, 1, 1],
-    //         vec![1, 1, 1, 1, 1, 1, 1000],
-    //    ];
+    // let grid = vec![
+    //     vec![5, 9, 2, 6, 4, 4, 9],
+    //     vec![4, 5, 7, 4, 6, 8, 8],
+    //     vec![8, 2, 9, 8, 8, 6, 5],
+    //     vec![2, 1, 1, 3, 5, 7, 1],
+    //     vec![1, 8, 1, 7, 1, 6, 8],
+    // ];
     println!("Grid generated in: {:.2?}", before.elapsed());
     let before = Instant::now();
     let solution = find_max_coins(grid);
@@ -31,7 +31,7 @@ fn main() {
 
 fn find_max_coins(grid: Vec<Vec<i16>>) -> Vec<i16> {
     let mut solution: Vec<i16> = vec![];
-    let mut prev_idx = Box::new(0usize);
+    let mut prev_idx = &mut 0usize;
 
     // Iterate through the grid
     for row in 0..grid.len() {
@@ -59,6 +59,7 @@ fn find_max_coins(grid: Vec<Vec<i16>>) -> Vec<i16> {
             // println!("row {}: best option at index {}, target value {}", row, prev_idx, current_row[*prev_idx]);
             continue;
         }
+        // For all subsequent rows, greedily pick the best path
         // println!("row {}: using prev index {}, target value {}", row, prev_idx, grid[row - 1][*prev_idx]);
         solution.push(prev_row_max(
             current_row,
@@ -71,17 +72,20 @@ fn find_max_coins(grid: Vec<Vec<i16>>) -> Vec<i16> {
 
 fn prev_row_max(current_row: &Vec<i16>, target: i16, prev_idx: &mut usize) -> i16 {
     let mut current_max: i16 = 0;
+    // Don't modify the heap value until we have a value to replace it with
     let mut local_max_idx: usize = *prev_idx;
     for col in 0..current_row.len() {
-        let value = current_row[col] + target - (*prev_idx as i16 - col as i16).abs(); // change fee
-                                                                                       // println!("calc: {} + {} - (|{} - {}|) = {}", current_row[col], target, *prev_idx as i16, col as i16, value);
+        // Determine value less change fee
+        let value = current_row[col] + target - (*prev_idx as i16 - col as i16).abs();
+        // println!("calc: {} + {} - (|{} - {}|) = {}", current_row[col], target, *prev_idx as i16, col as i16, value);
         if value > current_max {
             current_max = value;
             local_max_idx = col;
             // println!("new max {} at index {}", current_max, local_max_idx)
         }
     }
-    *prev_idx = local_max_idx; // Copy the final value back to the heap
+    // Copy the final value back into the borrowed variable
+    *prev_idx = local_max_idx;
     return current_row[*prev_idx];
 }
 
